9b1771868a341ff6529a4dee2dfa08f06d02e473
badgerine <sbu.matsini@gmail.com>
2021-04-07 00:15:34 +0200

    use Repository.save() method, cleanup unnecessary code

--- src/main/java/za/co/mamamoney/sbu/ussdGateway/persistence/SessionRepository.java
+++ src/main/java/za/co/mamamoney/sbu/ussdGateway/persistence/SessionRepository.java
@@ -1,13 +1,7 @@
 package za.co.mamamoney.sbu.ussdGateway.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 import za.co.mamamoney.sbu.ussdGateway.model.Session;
-
-import java.time.LocalDateTime;
 
 public interface SessionRepository extends JpaRepository<Session,String> {
 }
